00d49b4fbc82ac80aacc62317c906a9e4196abfe
Lepoldo Caballero <git@pitakill.net>
2019-03-01 22:32:48 -0600

    [feat] Get tweets dinamically

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import TuitList from '../Tuit/TuitList';
 
+import addTuit from '../../actions/action-add-tuit'
+
+const url = 'https://gist.githubusercontent.com/pitakill/1b350ebb47d8c07789d13d2cce83ca7e/raw/1942e914e42a08037e182aa737f030eaf21cc50a/tuits.json'
+
 class Home extends Component {
+  async componentDidMount() {
+    const responseRaw = await fetch(url)
+    const response = await responseRaw.json()
+    response.forEach(tuit => this.props.addTuit(tuit))
+  }
+
   render() {
     return (
       <div>
@@ -22,6 +33,8 @@ function mapStateToProps(state) {
   };
 }
 
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({addTuit}, dispatch)
+}
 
-
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
